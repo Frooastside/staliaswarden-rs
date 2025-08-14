40346b5c2fffdc71db4c9d01436155d758969f04
Romanos Tsouroplis <rom-dim@hotmail.com>
2025-08-14 22:50:33 +0200

    Changed token to user-pass

--- src/stalwart.js
+++ src/stalwart.js
@@ -2,22 +2,27 @@ import axios from 'axios';
 import config from './config.js';
 
 export async function addAliasToStalwart(alias) {
-  try {
-    await axios.post(
-      `${config.stalwartUrl}/aliases`,
+  const api = axios.create({
+    baseURL: config.stalwartUrl,
+    auth: {
+      username: config.stalwartUsername,
+      password: config.stalwartPassword
+    },
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  api
+    .post(
+      '/aliases',
       {
         alias,
         destinations: [config.forwardTo]
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${config.stalwartToken}`,
-          'Content-Type': 'application/json'
-        }
       }
-    );
-    console.log(`Alias ${alias} added to Stalwart`);
-  } catch (err) {
-    console.error(`Failed to add alias to Stalwart:`, err.response?.data || err);
-  }
+    )
+    .then(res => console.log(`Alias ${alias} added to Stalwart`))
+    .catch(err => {
+      console.error(`Failed to add alias to Stalwart:`, err.response?.data || err);
+    });
 }
